c995819f5bdb6d6c21988597797775b3f7169926
nachbr91 <nabero91@gmail.com>
2021-11-01 11:29:13 +0100

    Edit script.js: Add main character static img and movement

--- js/script.js
+++ js/script.js
@@ -4,8 +4,30 @@ window.onload = () => {
   const ctx = canvas.getContext('2d');
 
   //VARIABLES
-  const totalOfImages = 1;
+  const totalOfImages = 2;
   let counterForLoadedImages = 0;
+
+
+  // CLASSES
+
+  class MainCharacter {
+    constructor() {
+      this.x = 50;
+      this.y = 200
+      this.speedX = 0;
+      this.speedY = 0;
+      this.width = 80;
+      this.height = 140;
+    }
+
+    updatePosition ()  {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+  }
+
+  const mainCharacter = new MainCharacter(); // Create const of MainCharacter class
+
 
 
   //EVENT LISTENERS
@@ -13,43 +35,73 @@ window.onload = () => {
       startGame();
   };
 
+  document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      mainCharacter.speedX = 3;
+    } else if (event.key === 'ArrowLeft') {
+      mainCharacter.speedX = -3;
+    } else if (event.key === 'ArrowUp') {
+      mainCharacter.speedY = -3;
+    } else if (event.key === 'ArrowDown') {
+      mainCharacter.speedY = 3;
+    };
+  });
+
+  document.addEventListener('keyup', (event) => {
+    if (event.key === ('ArrowRight') || event.key === ('ArrowLeft')) {
+      mainCharacter.speedX = 0;
+    } else if (event.key === 'ArrowUp' || event.key === ('ArrowDown')) {
+      mainCharacter.speedY = 0;
+    };
+  });
+
 
 
   //FUNCTIONS
 
   const startGame = () => {
-      updateCanvas()
       generateImages();
-  }
+      updateCanvas()
+  };
 
   let backgroundImage = '';
-  // let mainCharacterImage = '';
+  let mainCharacterImage = '';
 
   const drawImages = (backgroundImage, mainCharacterImage) => {
     ctx.drawImage(backgroundImage, 0, 0, 800, 450);
-    ctx.fillRect(20, 200, 50, 100) // Main character container test. It would be replaced by mainCharacterImg
+    ctx.drawImage(mainCharacterImage, mainCharacter.x, mainCharacter.y, mainCharacter.width, mainCharacter.height);
   };
 
   const generateImages = () => {
     backgroundImage = new Image();
+    mainCharacterImage = new Image();
 
     backgroundImage.src = '../images/canvas_background.png';
+    mainCharacterImage.src = '/images/main_character_img/stand_1.png';
 
     backgroundImage.onload = () => {
       counterForLoadedImages++;
       if (counterForLoadedImages === totalOfImages) {
         drawImages(backgroundImage, mainCharacterImage);
-
-      }
+      };
     };
+
+    mainCharacterImage.onload = () => {
+      counterForLoadedImages++;
+      if (counterForLoadedImages === totalOfImages) {
+        drawImages(backgroundImage, mainCharacterImage);
+      };
+    };
+
   };
 
   const updateCanvas = () => {
-      if (counterForLoadedImages === totalOfImages) {
-          drawImages(backgroundImage);
-      }
-      requestAnimationFrame(updateCanvas);
-  }
+    mainCharacter.updatePosition();
+    if (counterForLoadedImages === totalOfImages) {
+        drawImages(backgroundImage, mainCharacterImage);
+    }
+    requestAnimationFrame(updateCanvas);
+  };
 
 
 };
